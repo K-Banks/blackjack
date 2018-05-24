d495a6726fb5389418a759f6b5a24ea8c80138ee
Garnett Puett and Kayl Eubanks <kayl@>
2018-05-24 11:07:07 -0700

    add frontend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,11 +17,61 @@ public class App {
 
             try {
                 Game blackjack = new Game();
-                Player player1 = new Player();
+                Player user = new Player();
+                Player dealer = new Player();
                 blackjack.createDeck();
-                blackjack.drawCard();
+                user.addCard(blackjack.drawCard());
+                dealer.addCard(blackjack.drawCard());
+                user.addCard(blackjack.drawCard());
+                dealer.addCard(blackjack.drawCard());
+                System.out.println("Your hand: " + user.getHand());
+                System.out.println("Your points: " + user.valueOfHand());
+                System.out.println("Dealer's hand: " + dealer.getHand().get(0));
 
-                String choice = bufferedReader.readLine();
+                boolean gameState = true;
+                while (gameState) {
+                    System.out.println("Do you hit or check?");
+                    String choice = bufferedReader.readLine().toLowerCase();
+                    if (choice.equals("hit")){
+                        user.addCard(blackjack.drawCard());
+                        System.out.println("Your hand: " + user.getHand());
+                        System.out.println("Your points: " + user.valueOfHand());
+                        if (user.valueOfHand() > 21) {
+                            System.out.println("You busted.");
+                            gameState = false;
+                        }
+                    } else {
+                        System.out.println("Dealer hand: " + dealer.getHand());
+                        System.out.println("Dealer points: " + dealer.valueOfHand());
+                        while (dealer.valueOfHand() < 17) {
+                            dealer.addCard(blackjack.drawCard());
+                            System.out.println("Dealer hand: " + dealer.getHand());
+                            System.out.println("Dealer points: " + dealer.valueOfHand());
+                        }
+                        if (dealer.valueOfHand() > 21) {
+                            System.out.println("The dealer busted. You win.");
+                            gameState = false;
+                        } else if (dealer.valueOfHand() == 21 && user.valueOfHand() == 21) {
+                            System.out.println("Tie game.");
+                            gameState = false;
+                        } else if (dealer.valueOfHand() == 21) {
+                            System.out.println("Dealer wins.");
+                            gameState = false;
+                        } else if (dealer.valueOfHand() > user.valueOfHand()) {
+                            System.out.println("Dealer wins.");
+                            gameState = false;
+                        } else {
+                            System.out.println("You win.");
+                            gameState = false;
+                        }
+                    }
+                }
+
+                System.out.println("Would you like to play again? Y or N");
+                String input = bufferedReader.readLine().toLowerCase();
+                if (input.equals("n")) {
+                    playing = false;
+                }
 
 
             } catch (IOException e) {
